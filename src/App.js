1eeb5d9f183ab45cd8eb909e20c8e4de42584608
Eduardo <jeduardorjx@gmail.com>
2019-10-08 16:08:20 -0600

    Setup App component and intital fetch set up Co-authored-by: Noah Gibson <noah.gibson99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { apiCalls } from './apiCalls'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    const swapiFilmsUrl = 'https://swapi.co/api/films';
+    apiCalls(swapiFilmsUrl)
+  }
+
+  render() {
+    return (
+      <main>
+        hello
+      </main>
+    )
+  }
 }
 
 export default App;
-
-
-
-
