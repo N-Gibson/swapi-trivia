53d105478d54d0be745c4079364247033a3ae295
Noah Gibson <noah.gibson99@gmail.com>
2019-10-08 17:36:21 -0600

    Set state to character objects
    Co-authored-by: Eduardo Rodriguez <jeduardorjx@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      movies: [],
+      loading: true
     }
   }
 
   componentDidMount() {
     const swapiFilmsUrl = 'https://swapi.co/api/films';
     apiCalls(swapiFilmsUrl)
+      .then(data => this.setState({ movies: data, loading: false }))
   }
 
   render() {
+    console.log(this.state)
     return (
       <main>
         hello
