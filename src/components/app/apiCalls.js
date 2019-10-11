f1164e884bd5c436dad75cfe3afdde89d425b74e
Noah Gibson <noah.gibson99@gmail.com>
2019-10-11 17:37:25 -0600

    Add isFavorite state to characters

--- src/components/app/apiCalls.js
+++ src/components/app/apiCalls.js
@@ -28,8 +28,9 @@ const getCharacters = (characters) => {
     })
     .then(char => {
       const {films, homeworld, name, species } = char
+      let isFavorite = false
       return getSpecies(species)
-        .then(species => ({films, homeworld, name, species}))
+        .then(species => ({films, homeworld, name, species, isFavorite}))
     })
   })
   return Promise.all(charactersData)
