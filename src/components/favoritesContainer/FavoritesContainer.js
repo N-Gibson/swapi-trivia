3fa20a2d23de3ddd66990a2460e9208b049db096
Noah Gibson <noah.gibson99@gmail.com>
2019-10-12 14:06:36 -0600

    Add basic styling to favorites container

--- src/components/favoritesContainer/FavoritesContainer.js
+++ src/components/favoritesContainer/FavoritesContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CharacterCard from '../characters/CharacterCard';
+import './FavoritesContainer.scss'
 
 const FavoritesContainer = (props) => {
   let favoriteCharacters = props.characters.reduce((acc, character) => {
@@ -12,7 +13,7 @@ const FavoritesContainer = (props) => {
   let character = favoriteCharacters.map(character => <CharacterCard character={character} />)
   return (
     <section className='favorite-characters'>
-      <h2>Favorite Characters</h2>
+      <h2 className='favorite-characters-h2'>Favorite Characters</h2>
       {character}
     </section>
   )
