6b74ffd733e6099eb02570939b2f9d72561fe08d
Eduardo <jeduardorjx@gmail.com>
2019-10-13 16:08:39 -0600

    Finish organizing splash file

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -4,53 +4,49 @@ import './splash.scss'
 import jediBold from '../../icons/jedi_bold.svg'
 import sithBold from '../../icons/sith_bold.svg'
 
-
 const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, name, favQuote }) => {
   let handleJumpToLightSpeedBtn;
     if(name !== '' & favQuote !== '') {
       handleJumpToLightSpeedBtn = (
       <Link to='/movies'>
         <button 
-        className='favquote-btn' 
-        id={orderColor}
-        type='button' 
-        onClick={e => {
-          // let name = e.target.parentElement.firstChild.value
-          // let quote = e.target.parentElement.childNodes[1].value
-          // let rank = e.target.parentElement.childNodes[2].value
-          // props.handleForm(name, quote, rank)
-        }
-      }>Jump to Light Speed</button>
+        className='jump-to-light-speeb-btn' 
+        id={orderColor} 
+        type='button' >Jump to Light Speed</button>
       </Link>)
     }
 
   return (
     <section className='splash-container'>
       <div className='splash-div' >
-      <h1 className='splash-starwars-h1' id={orderColor} >Star</h1>
-      <h2 className='splash-starwars-h2'>A Trivia Story</h2>
-      <h1 className='splash-starwars-h1' id={orderColor} >Wars</h1>
+        <h1 className='splash-starwars-h1' id={orderColor} >Star</h1>
+        <h2 className='splash-starwars-h2'>A Trivia Story</h2>
+        <h1 className='splash-starwars-h1' id={orderColor} >Wars</h1>
       </div>
       <form id={orderColor}>
         <input type='text' 
           placeholder='Enter Name'
           name='name' 
           onChange={event => handleFormChange(event)}></input>
-        <h2 className='splash-starwars-h2' id={orderColor} >Jedi or Sith</h2>
+        <h2 className='splash-starwars-h2' 
+          id={orderColor} >Jedi or Sith</h2>
         <div className='jedi-or-sith-div'>
           <button 
             className='order-button jedi-btn' 
             type='button' 
             onClick={event => handleOrderColor(event)}>
-              <img className='order-icon' src={jediBold} />
+              <img className='order-icon' 
+                src={jediBold} />
           </button>
           <button className='order-button sith-btn' 
-          type='button'
-          onClick={event => handleOrderColor(event)}>
-            <img className='order-icon' src={sithBold} />
+            type='button'
+            onClick={event => handleOrderColor(event)}>
+            <img className='order-icon' 
+              src={sithBold} />
           </button>
         </div>
-
+        <h2 className='splash-starwars-h2' 
+          id={orderColor} >Rank</h2>
         <select id={orderColor}>
           <option value={orderRank[0]}>{orderRank[0]}</option>
           <option value={orderRank[1]}>{orderRank[1]}</option>
@@ -60,7 +56,6 @@ const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, nam
           placeholder='Enter Favorite Quote' 
           name='favQuote'
           onChange={event => handleFormChange(event)}></input>
-
         {handleJumpToLightSpeedBtn}
       </form>
     </section>
