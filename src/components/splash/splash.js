6e66e2070605566fd12b1c6355d66d0a878ec3ac
Eduardo <jeduardorjx@gmail.com>
2019-10-13 18:31:05 -0600

    Pass through handleFormChange as a prop

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -4,7 +4,7 @@ import './splash.scss'
 import jediBold from '../../icons/jedi_bold.svg'
 import sithBold from '../../icons/sith_bold.svg'
 
-const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, name, favQuote }) => {
+const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, name, favQuote, setRank }) => {
   let handleJumpToLightSpeedBtn;
     if(name !== '' & favQuote !== '') {
       handleJumpToLightSpeedBtn = (
@@ -12,16 +12,19 @@ const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, nam
         <button 
         className='jump-to-light-speeb-btn' 
         id={orderColor} 
-        type='button'>Jump to Light Speed</button>
+        type='button'
+        >Jump to Light Speed</button>
       </Link>)
     }
 
   return (
     <section className='splash-container'>
       <div className='splash-div' >
-        <h1 className='splash-starwars-h1' id={orderColor} >Star</h1>
+        <h1 className='splash-starwars-h1' 
+          id={orderColor} >Star</h1>
         <h2 className='splash-starwars-h2'>A Trivia Story</h2>
-        <h1 className='splash-starwars-h1' id={orderColor} >Wars</h1>
+        <h1 className='splash-starwars-h1' 
+          id={orderColor} >Wars</h1>
       </div>
       <form id={orderColor}>
         <input type='text' 
@@ -49,7 +52,8 @@ const Splash = ({ handleFormChange, handleOrderColor, orderColor, orderRank, nam
         </div>
         <h2 className='splash-starwars-h2' 
           id={orderColor} >Rank</h2>
-        <select id={orderColor}>
+        <select id={orderColor} 
+          onChange={event => setRank(event)}>
           <option value={orderRank[0]}>{orderRank[0]}</option>
           <option value={orderRank[1]}>{orderRank[1]}</option>
           <option value={orderRank[2]}>{orderRank[2]}</option>
