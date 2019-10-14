611ab945004d8f25f51a1fe9021fd4211a07afe3
Eduardo <jeduardorjx@gmail.com>
2019-10-13 21:31:29 -0600

    Set same move data set for more cards to display while working on project

--- src/components/app/SampleData.js
+++ src/components/app/SampleData.js
@@ -25,6 +25,168 @@ const SampleData = [{
       name: "Dud Bolt",
       species: "Vulptereen"
     }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
+  }, {
+    episode_id: 1,
+    image: "https://vignette.wikia.nocookie.net/starwars/images/7/75/EPI_TPM_poster.png/revision/latest?cb=20130822171446",
+    release_date: "1999-05-19",
+    title: "The Phantom Menace",
+    characters: [{
+      films: ["The Phantom Menace"],
+      homeworld: ["Mars", "234567890"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Qui-Gon Jinn",
+      species: "Human"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Naboo", "4500000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Roos Tarpals",
+      species: "Gungan"
+    }, {
+      films: ["The Phantom Menace"],
+      homeworld: ["Vulpter", "421000000"],
+      id: Date.now(),
+      isFavorite: false,
+      name: "Dud Bolt",
+      species: "Vulptereen"
+    }]
   }]
 
   export default SampleData
