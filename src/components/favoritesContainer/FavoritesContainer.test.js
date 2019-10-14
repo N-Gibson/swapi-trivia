040557321165789250235af9abb02742a6d6bc9a
Noah Gibson <noah.gibson99@gmail.com>
2019-10-13 21:38:18 -0600

    Add snapshot test

--- src/components/favoritesContainer/FavoritesContainer.test.js
+++ src/components/favoritesContainer/FavoritesContainer.test.js
@@ -5,6 +5,12 @@ import FavoritesContainer from './FavoritesContainer';
 describe('Favorites Container', () => {
   let wrapper
   beforeEach(() => {
+    // need to pass props to container
+    // mock or actually pass props?
     wrapper = shallow(<FavoritesContainer />)
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper.toMatchSnapshot());
   })
 });
