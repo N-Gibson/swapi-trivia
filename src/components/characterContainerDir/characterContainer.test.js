827dd47e5e500b29ca31af62d988e5358eed1e25
Noah Gibson <noah.gibson99@gmail.com>
2019-10-13 21:45:52 -0600

    Add all test boiler plates

---
+++ src/components/characterContainerDir/characterContainer.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import characterContainer from './characterContainer';
+
+describe('Character Container', () => {
+  let wrapper
+  beforeEach(() => {
+
+  })
+
+  it('', () => {
+    
+  })
+})
---
+++ src/components/characters/characters.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import characters from './characters';
+
+describe('Character Container', () => {
+  let wrapper
+  beforeEach(() => {
+
+  })
+
+  it('', () => {
+    
+  })
+})
