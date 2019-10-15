4d05268731b798ede61c757e06946b70890a4f35
Noah Gibson <noah.gibson99@gmail.com>
2019-10-15 14:45:20 -0600

    Updates

---
+++ precache-manifest.da5fd37e9c90848ef4045da209e23fa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7269d814190edd10b728b6207482e2f2",
+    "url": "/swapi-trivia/index.html"
+  },
+  {
+    "revision": "98a1352e04ad2d2f8729",
+    "url": "/swapi-trivia/static/css/main.3c687c18.chunk.css"
+  },
+  {
+    "revision": "0affb8d0386317f8297d",
+    "url": "/swapi-trivia/static/js/2.c723e9df.chunk.js"
+  },
+  {
+    "revision": "98a1352e04ad2d2f8729",
+    "url": "/swapi-trivia/static/js/main.73fa3752.chunk.js"
+  },
+  {
+    "revision": "4d91dc8747153c95bca8",
+    "url": "/swapi-trivia/static/js/runtime-main.ac423d25.js"
+  },
+  {
+    "revision": "fa01db36ef434ca5e93ae2b0957963af",
+    "url": "/swapi-trivia/static/media/jedi_bold.fa01db36.svg"
+  },
+  {
+    "revision": "50ee4c43b80cc43c2ebc52e266bcd23b",
+    "url": "/swapi-trivia/static/media/sith_bold.50ee4c43.svg"
+  },
+  {
+    "revision": "a00bdb76adaddaed08f4d0118e00fd6a",
+    "url": "/swapi-trivia/static/media/starjedi-webfont.a00bdb76.woff"
+  }
+]);
