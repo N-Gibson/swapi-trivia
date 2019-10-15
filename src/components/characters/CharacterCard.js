e827b944a73b11e3b41b2cb900465f94ae80bd52
Eduardo <jeduardorjx@gmail.com>
2019-10-14 20:51:24 -0600

    Destructure props and had a div to use as a sperator between Char. info

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -2,23 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './characters.scss';
 
-const Character = (props) => {
-  let { films, homeworld, name, species } = props.character
+const Character = ({ character, favoriteCharacter, orderColor}) => {
+  let { films, homeworld, name, species } = character
   if(species === undefined) {
     species = 'Unknown'
   }
   let characterFilms = films.map(film => <p>{film}</p>)
 
   return (
-    <article className='character-card'>
-      <button type='button' onClick={props.favoriteCharacter}> ⭐</button>
-      <p>{name}</p>
-      <p>{homeworld[0]}</p>
-      <p>{homeworld[1]}</p>
-      <p>{species}</p>
+    <article className='character-card'
+    id={orderColor}>
+      <h1 className='character-name-h1'
+        id={orderColor}>{name}</h1>
+        <div className='line'
+          id={orderColor}></div>
+      <h2 className='character-h2'
+        id={orderColor}>Homeworld:</h2>
+      <h2 className='character-h2'
+        id={orderColor}>{homeworld[0]}</h2>
+      <h2 className='character-h2'
+        id={orderColor}>Population:</h2>
+      <h2 className='character-h2'
+        id={orderColor}>{homeworld[1]}</h2>
+        <div className='line'
+          id={orderColor}></div>
+
+      <h2 className='character-h2'
+        id={orderColor}>Species:</h2>
+      <h2 className='character-h2'
+        id={orderColor}>{species}</h2>
+        <div className='line'
+          id={orderColor}></div>
+        <h2 className='character-h2'
+        id={orderColor}>Appears In:</h2>
       <div>
-        {characterFilms}
+        <h2 className='character-movie-h2'
+          id={orderColor}>{characterFilms}</h2>
       </div>
+      <button type='button' 
+        className='favorite-character'
+        id={orderColor}
+        onClick={favoriteCharacter}>Favorite Character</button>
     </article>
   )
 }
