eef2d721248340a4dd76e1d1a4709ca27f868d68
Noah Gibson <noah.gibson99@gmail.com>
2019-10-15 08:54:55 -0600

    Modify charicters to only show first ten characters

--- src/components/characterContainerDir/characterContainer.js
+++ src/components/characterContainerDir/characterContainer.js
@@ -5,7 +5,7 @@ import Nav from '../nav/nav'
 
 
 const CharacterContainer = ({ characters, favoriteCharacter, orderColor, name, favQuote, rank, movieNumber, movieTitle, scroll}) => {
-  const character = characters.map(character => <CharacterCard character={character} 
+  const character = characters.slice(0, 10).map(character => <CharacterCard character={character} 
     favoriteCharacter={favoriteCharacter}
     orderColor={orderColor} />)
 
